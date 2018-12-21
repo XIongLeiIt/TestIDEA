910b447e310f50a44ddc1a1fc214c37bdb65b9bc
shilin <1843416717@qq.com>
2018-12-21 17:16:18 +0800

    delete test method;

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import org.junit.Test;
 
 public class Main {
-
-    @Test
-    public void testHelloWrold() {
-        System.out.println("HelloWorld!");
-    }
 
     public static void main(String[] args) {
         System.out.println("test");
